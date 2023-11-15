6e6311abc604d7389cb1641d26365a39b0d888d9
Mario Kohlhoff <mario.kohlhoff@edag.com>
2023-11-15 21:51:15 +0100

    Fix tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,15 +3,23 @@ import Home from '../pages/index'
 import '@testing-library/jest-dom'
 
 test('renders the component with the correct data', () => {
-  const allCommunitiesData = [
-    { uuid: '95C87636-8320-4155-AEEF-1BA973C872CA', name: 'Community 1', createdAt: '2022-01-01' },
-    { uuid: '15C87636-8320-4155-AEEF-1BA973C872CA', name: 'Community 2', createdAt: '2022-01-02' },
-  ];
+    const allCommunitiesData = [
+        {
+            uuid: '95C87636-8320-4155-AEEF-1BA973C872CA',
+            name: 'Community 1',
+            createdAt: '2022-01-01',
+        },
+        {
+            uuid: '15C87636-8320-4155-AEEF-1BA973C872CA',
+            name: 'Community 2',
+            createdAt: '2022-01-02',
+        },
+    ]
 
-  const { getByText } = render(
-      <Home allCommunitiesData={allCommunitiesData} />
-  );
+    const { getByText } = render(
+        <Home allCommunitiesData={allCommunitiesData} allItemsData={[]} />
+    )
 
-  expect(getByText('Community 1')).toBeInTheDocument();
-  expect(getByText('Community 2')).toBeInTheDocument();
-});
+    expect(getByText('Community 1')).toBeInTheDocument()
+    expect(getByText('Community 2')).toBeInTheDocument()
+})
--- setupTests.ts
+++ setupTests.ts
@@ -1,38 +1,51 @@
-import { cleanup } from "@testing-library/react";
-import "@testing-library/jest-dom";
-import { afterEach } from "node:test";
+import { cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import { afterEach } from 'node:test'
 
-afterEach(cleanup);
+afterEach(cleanup)
 
-jest.mock("next-auth/react", () => {
-  return {
-    useSession: jest.fn(() => {
-      return {
-        user: {
-          email: "test@example.com",
-          name: "Test User",
-        },
-        status: "authenticated",
-        signin: jest.fn(),
-        signout: jest.fn(),
-      };
+jest.mock('next-auth/react', () => {
+    return {
+        useSession: jest.fn(() => {
+            return {
+                user: {
+                    email: 'test@example.com',
+                    name: 'Test User',
+                },
+                status: 'authenticated',
+                signin: jest.fn(),
+                signout: jest.fn(),
+            }
+        }),
+    }
+})
+
+jest.mock('next-auth', () => jest.fn())
+
+jest.mock('next-auth/next', () => {
+    return {
+        unstable_getServerSession: jest.fn(() => {
+            return {
+                accessToken: 'token',
+                user: {
+                    email: 'test@example.com',
+                    name: 'Test User',
+                },
+                status: 'authenticated',
+            }
+        }),
+    }
+})
+
+jest.mock('next/router', () => ({
+    ...jest.requireActual('next/router'),
+    useRouter: jest.fn(() => {
+        return {
+            push: jest.fn(), // Mock the push method
+            pathname: '/',
+            route: '/',
+            query: {},
+            asPath: '/',
+        }
     }),
-  };
-});
-
-jest.mock("next-auth", () => jest.fn());
-
-jest.mock("next-auth/next", () => {
-  return {
-    unstable_getServerSession: jest.fn(() => {
-      return {
-        accessToken: "token",
-        user: {
-          email: "test@example.com",
-          name: "Test User",
-        },
-        status: "authenticated",
-      };
-    }),
-  };
-});
+}))
